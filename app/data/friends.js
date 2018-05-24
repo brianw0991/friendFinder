602d2ea6753ab600972744a59b1499ecad906d59
Brian Witte <brianw0991@gmail.com>
2018-05-24 15:38:17 -0500

    added friends array and two 'test friends'

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+//==============================================
+// Array to hold all friends that use the app
+// some test friends are already included
+//==============================================
+
+var friendArray = [
+    {
+        name: "Tom",
+        photo: "<a href='https://pbs.twimg.com/profile_images/1237550450/mstom.jpg' target='_blank'><img src='https://pbs.twimg.com/profile_images/1237550450/mstom.jpg' alt='Myspace Tom'></a>",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+
+    },
+    {
+        name: "Christopher",
+        photo: "<a href='https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjM0MDAwNjcyMjY4/christopher-walken-9521854-1-402.jpg' target='_blank'><img src='https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjM0MDAwNjcyMjY4/christopher-walken-9521854-1-402.jpg' alt='Christopher Walken'></a>",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+
+    }
+]
+
+
+
+module.exports= friendArray;
