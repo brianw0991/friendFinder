9604332a56983e8c3fe6bceacf393f02218061da
Brian Witte <brianw0991@gmail.com>
2018-05-24 13:51:33 -0500

    built server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParse = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8000
+
+app.use(bodyParse.urlencoded( {extended: true}));
+app.use(bodyParse.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("application listening on PORT: " + PORT);
+});
